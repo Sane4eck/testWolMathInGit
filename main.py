f56b68ace292d7994d9780a1269f0936c4e3f3de
Sane4eck <77103844+Sane4eck@users.noreply.github.com>
2026-03-19 17:54:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
 # main.py
-import os, argparse
+import os
+import argparse
+import time
 
 ap = argparse.ArgumentParser()
-# ap.add_argument("--system", default="sys_C_0")
-ap.add_argument("--system", default="sys_with_C")
+ap.add_argument("--system", default="sys_wolmath")
+ap.add_argument("--excel", default="_1_InitialDataSV3.xlsx")
+ap.add_argument("--dt", type=float, default=1e-7)
+ap.add_argument("--end-time", type=float, default=1.0)
 args = ap.parse_args()
 
 os.environ["DYNAMICS_SYSTEM"] = args.system
-
 
 from core.state import State
 from core.system import Params, initial_y
 from core.model import HydraulicModel
 from core.result import Result
-import time
-
-dt = 1e-7
-endTime = 1.0
-
-state = State(time=0.0, y=initial_y())
-params = Params()
-model = HydraulicModel()
-
-t0 = time.perf_counter()
-t_arr, y_arr, dy_arr, aux_arr = model.simulate(state, params, dt, endTime, countPoint=3000, backend="numba")
-res = Result(t_arr, y_arr, dy_arr, aux_arr)
-t1 = time.perf_counter()
-print(f"Execution time: {t1-t0:.2f} s")
-
 from plot import plot_results
-plot_results(res.data)
-
-import time
-run_id = time.strftime("%Y%m%d_%H%M%S")
-
 from core.plotting import save_all_plots
-save_all_plots(res, out_dir=f"out/plots/{run_id}", show=False)
-
 from core.export_excel import save_to_excel
-save_to_excel(res, f"out/data/{run_id}.xlsx")
+
+def main():
+    params = Params.from_excel(args.excel)
+    state = State(time=0.0, y=initial_y(params))
+    model = HydraulicModel()
+
+    t0 = time.perf_counter()
+    t_arr, y_arr, dy_arr, aux_arr = model.simulate(
+        state, params, args.dt, args.end_time, countPoint=3000, backend="numba"
+    )
+    res = Result(t_arr, y_arr, dy_arr, aux_arr)
+    t1 = time.perf_counter()
+
+    print(f"Execution time: {t1 - t0:.2f} s")
+
+    plot_results(res.data)
+
+    run_id = time.strftime("%Y%m%d_%H%M%S")
+    save_all_plots(res, out_dir=f"out/plots/{run_id}", show=False)
+    save_to_excel(res, f"out/data/{run_id}.xlsx")
+
+if __name__ == "__main__":
+    main()
